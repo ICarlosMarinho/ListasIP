f9741cb5b649ae626fc76fe2a362e49750144335
Carlos Marinho <carlos.marinho20@outlook.com>
2017-06-09 21:45:37 -0300

    ...

--- lista02/AplicacaoRepositorioLivros.java
+++ lista02/AplicacaoRepositorioLivros.java
@@ -1,7 +1,7 @@
 package lista02;
 import java.util.Scanner;
 
-public class ApricacaoRepositorioLivros 
+public class AplicacaoRepositorioLivros 
 {
     public static void main(String[] args)
     {
